e9bf246fd135a22ea93ba5d1abf4bb0881fa625d
zsh <zsh__zsh@outlook.com>
2023-04-18 19:54:53 +0800

    noname_1

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
+def index(request):
+    return render(request,'learning_logs/index.shtml')
+def topics(request):
+    topics=Topic.objects.order_by('date_added')
+    comtext={'topics':topics}
+    return render(request,'learning_logs/topics.shtml',comtext)
